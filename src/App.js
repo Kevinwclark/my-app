861fc1d4400b13a6fc5dee8134eb0b655abbd615
Kevin Clark <kevinwilliamclark@gmail.com>
2021-01-09 12:19:14 -0600

    able to fetch the movies, need to extract out the api key and set up the search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+    const [movies, setMovies] = useState([])
+    console.log(movies)
+
+    function fetchData() {
+        fetch('http://www.omdbapi.com/?i=tt3896198&apikey=c45d5077')
+            .then(response => response.json())
+            .then(data => setMovies(data));
+    };
+    
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={fetchData} type="submit" value="SEARCH">Movies</button>
     </div>
   );
 }
